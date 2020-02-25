3f2afdc6217bfd924de641b511c3fea742a4f928
ruben3185 <ruben3185@hotmail.com>
2020-02-24 20:51:35 -0500

    requirements, url and router

---
+++ baseDRF/router.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+
+
+
+router = routers.DefaultRouter(trailing_slash=True)
+
+
+# Ejemplo de la url de tu vista
+#router.register(r'link', LinkViewSet)
--- baseDRF/urls.py
+++ baseDRF/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from .router import router
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
+    path('auth/', include('rest_framework.urls')),
 ]
+
+
+if settings.STATIC_ROOT:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
